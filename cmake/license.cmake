18791330462395b13516af631f8d084c8a91b81d
Dan Keenan <dk@dankeenan.org>
2020-06-07 11:17:25 -0400

    Build linux packages

---
+++ cmake/license.cmake
@@ -0,0 +1,22 @@
+# Build the license files as HTML, RTF, and plain text
+find_program(PANDOC_PATH NAMES pandoc)
+if (NOT PANDOC_PATH)
+    message(FATAL_ERROR "Could not find pandoc, required for building installers")
+endif ()
+
+message(STATUS "Creating license files")
+execute_process(COMMAND ${PANDOC_PATH}
+    "--standalone" "--self-contained" "--metadata" "pagetitle='GNU GENERAL PUBLIC LICENSE, Version 3'" "--from" "commonmark" "--to" "html" "${PROJECT_SOURCE_DIR}/LICENSE.md"
+    OUTPUT_FILE "${PROJECT_BINARY_DIR}/LICENSE.html"
+    ENCODING "UTF8"
+    )
+execute_process(COMMAND ${PANDOC_PATH}
+    "--standalone" "--self-contained" "--from" "commonmark" "--to" "rtf" "${PROJECT_SOURCE_DIR}/LICENSE.md"
+    OUTPUT_FILE "${PROJECT_BINARY_DIR}/LICENSE.rtf"
+    ENCODING "UTF8"
+    )
+execute_process(COMMAND ${PANDOC_PATH}
+    "--standalone" "--self-contained" "--from" "commonmark" "--to" "plain" "${PROJECT_SOURCE_DIR}/LICENSE.md"
+    OUTPUT_FILE "${PROJECT_BINARY_DIR}/LICENSE.txt"
+    ENCODING "UTF8"
+    )
