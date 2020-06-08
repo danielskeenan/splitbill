3607b7e041ee0f7c982e179a278dfa2826899f08
Dan Keenan <dk@dankeenan.org>
2020-06-08 18:06:48 -0400

    Fix some dependencies

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,5 +1,14 @@
 install(TARGETS splitbill RUNTIME BUNDLE)
 
+set(CPACK_PACKAGE_VENDOR "${PROJECT_AUTHOR}")
+set(CPACK_PACKAGE_CONTACT "${PROJECT_AUTHOR} <dk@dankeenan.org>")
+set(CPACK_PACKAGE_ICON "${PROJECT_SOURCE_DIR}/resources/app-icon.svg")
+set(CPACK_PACKAGE_CHECKSUM "MD5")
+include("${CMAKE_CURRENT_LIST_DIR}/license.cmake")
+set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_BINARY_DIR}/LICENSE.html")
+set(CPACK_PACKAGE_EXECUTABLES "splitbill;Split Bill")
+set(CPACK_MONOLITHIC_INSTALL On)
+
 # Linux metadata
 if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
     include(GNUInstallDirs)
@@ -12,29 +21,22 @@ if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
     install(FILES "${PROJECT_SOURCE_DIR}/resources/app-icon.svg"
         RENAME "splitbill.svg"
         DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/scalable/apps")
+
+    set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS On)
+    set(CPACK_DEBIAN_PACKAGE_SECTION "misc")
 endif ()
 
 # Windows metadata
 if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
     set(CPACK_NSIS_DISPLAY_NAME "Split Bill")
     set(CPACK_NSIS_PACKAGE_NAME "Split Bill")
+    set(CPACK_WIX_LICENSE_RTF "${PROJECT_BINARY_DIR}/LICENSE.rtf")
     set(CMAKE_INSTALL_UCRT_LIBRARIES TRUE)
     include(InstallRequiredSystemLibraries)
-
-    # Bundle Qt runtime
-    include("${CMAKE_CURRENT_LIST_DIR}/Windeployqt.cmake")
-    windeployqt(splitbill qt)
+#
+#    # Bundle Qt runtime
+#    include("${CMAKE_CURRENT_LIST_DIR}/Windeployqt.cmake")
+#    windeployqt(splitbill qt)
 endif ()
-
-set(CPACK_PACKAGE_VENDOR "${PROJECT_AUTHOR}")
-set(CPACK_PACKAGE_CONTACT "${PROJECT_AUTHOR} <dk@dankeenan.org>")
-set(CPACK_PACKAGE_ICON "${PROJECT_SOURCE_DIR}/resources/app-icon.svg")
-set(CPACK_PACKAGE_CHECKSUM "MD5")
-include("${CMAKE_CURRENT_LIST_DIR}/license.cmake")
-set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_BINARY_DIR}/LICENSE.html")
-set(CPACK_PACKAGE_EXECUTABLES splitbill;"Split Bill")
-set(CPACK_MONOLITHIC_INSTALL On)
-set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS On)
-set(CPACK_DEBIAN_PACKAGE_SECTION "misc")
 
 include(CPack)
