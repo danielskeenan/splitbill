e866293d7d57802e7ecf740cc85da504d8d32d7e
Dan Keenan <dk@dankeenan.org>
2021-01-11 13:55:07 -0500

    Create new Money class

---
+++ include/lib/Money.h
@@ -0,0 +1,61 @@
+/**
+ * @file Money.h
+ *
+ * @author dankeenan
+ * @date 1/11/21
+ * @copyright (c) 2021 Dan Keenan
+ */
+
+#ifndef SPLITBILL_INCLUDE_LIB_MONEY_H_
+#define SPLITBILL_INCLUDE_LIB_MONEY_H_
+
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include "Currency.h"
+
+namespace splitbill {
+
+/**
+ * Store monetary values without floating-point math
+ */
+class Money {
+ public:
+  explicit Money(const double &value, Currency::Info currency);
+
+  explicit Money(const double &value, Currency::Code currency) :
+      Money(value, Currency::Get(currency)) {}
+
+  explicit Money(const double &value, const std::string &currency) :
+      Money(value, Currency::Get(currency)) {}
+
+  [[nodiscard]] double GetValue() const;
+  [[nodiscard]] const Currency::Info &GetCurrency() const;
+
+  [[nodiscard]] bool operator==(const Money &rhs) const;
+  [[nodiscard]] bool operator!=(const Money &rhs) const;
+  [[nodiscard]] bool operator<(const Money &rhs) const;
+  [[nodiscard]] bool operator>(const Money &rhs) const;
+  [[nodiscard]] bool operator<=(const Money &rhs) const;
+  [[nodiscard]] bool operator>=(const Money &rhs) const;
+
+  [[nodiscard]] Money operator+(const Money &rhs) const;
+  [[nodiscard]] Money operator+(const double &rhs) const;
+  [[nodiscard]] Money operator-(const Money &rhs) const;
+  [[nodiscard]] Money operator-(const double &rhs) const;
+  [[nodiscard]] Money operator*(const Money &rhs) const;
+  [[nodiscard]] Money operator*(const double &rhs) const;
+  [[nodiscard]] Money operator/(const Money &rhs) const;
+  [[nodiscard]] Money operator/(const double &rhs) const;
+
+ private:
+  using Decimal = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<50>>;
+
+  Currency::Info currency_;
+  Decimal value_;
+
+  explicit Money(Decimal value, Currency::Info currency) :
+      currency_(std::move(currency)), value_(std::move(value)) {}
+};
+
+} // splitbill
+
+#endif //SPLITBILL_INCLUDE_LIB_MONEY_H_
---
+++ src/lib/Money.cpp
@@ -0,0 +1,77 @@
+/**
+ * @file Money.cpp
+ *
+ * @author dankeenan
+ * @date 1/11/21
+ * @copyright (c) 2021 Dan Keenan
+ */
+
+#include "Money.h"
+#include <cmath>
+#include <stdexcept>
+
+namespace splitbill {
+
+Money::Money(const double &value, Currency::Info currency) :
+    currency_(std::move(currency)), value_(value) {}
+
+double Money::GetValue() const {
+  return std::round((value_ * currency_.multiplier).convert_to<double>()) / currency_.multiplier;
+}
+
+const Currency::Info &Money::GetCurrency() const {
+  return currency_;
+}
+
+bool Money::operator==(const Money &rhs) const {
+  return currency_ == rhs.currency_ &&
+      value_ == rhs.value_;
+}
+
+bool Money::operator!=(const Money &rhs) const {
+  return !(rhs == *this);
+}
+
+bool Money::operator<(const Money &rhs) const {
+  if (currency_ != rhs.currency_) {
+    throw std::invalid_argument("Can only compare money with the same currency");
+  }
+  return value_ < rhs.value_;
+}
+
+bool Money::operator>(const Money &rhs) const {
+  return rhs < *this;
+}
+
+bool Money::operator<=(const Money &rhs) const {
+  return !(rhs < *this);
+}
+
+bool Money::operator>=(const Money &rhs) const {
+  return !(*this < rhs);
+}
+
+#define MONEY_OP(op) \
+  Money Money::operator op(const Money &rhs) const { \
+    if (currency_ != rhs.currency_) { \
+      throw std::invalid_argument("Can only operate on money with the same currency"); \
+    } \
+    return Money(value_ op rhs.value_, currency_); \
+  } \
+  \
+  Money Money::operator op(const double &rhs) const { \
+    return Money(value_ op rhs, currency_); \
+  } \
+
+
+MONEY_OP(+)
+
+MONEY_OP(-)
+
+MONEY_OP(*)
+
+MONEY_OP(/)
+
+#undef MONEY_OP
+
+} // splitbill
